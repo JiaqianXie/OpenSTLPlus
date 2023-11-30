c0e468bcaccea5c726f5d0aff4764ec65d776ac4
Brendan Do <brendan.do@csiro.au>
2023-12-01 06:18:49 +0800

    update loading best model during test phase, removed early stop, create build env bash script

--- install_env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-conda create -n openstlplus -y python=3.9
-conda activate openstlplus
-conda install -y pytorch torchvision pytorch-cuda=11.8 -c pytorch -c nvidia
-pip3 install hickle decord fvcore lpips nni pandas tqdm wandb --quiet
-pip3 install -e .
