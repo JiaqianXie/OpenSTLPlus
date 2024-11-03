30915dbb3d0442ec3a9f75a0b550c9f1b0e8b29d
Yang Li <li309@calculus-ph.nexus.csiro.au>
2024-11-04 00:47:16 +1000

    update install bash script

--- build_env.sh
+++ build_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# conda create -n OpenSTLPlus -y python=3.10
+# conda create -n OpenSTLPlus -y python=3.9
 # conda activate OpenSTLPlus
-conda install -y pytorch torchvision pytorch-cuda=12.1 -c pytorch -c nvidia
+conda install pytorch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 pytorch-cuda=12.1 -c pytorch -c nvidia
 conda install -y lightning -c conda-forge
 conda install -y -c conda-forge xarray dask netCDF4 bottleneck
 python -m pip install timm==0.6.11 --quiet
